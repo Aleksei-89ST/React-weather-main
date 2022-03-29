bd800597532b5813edd5e051236e1a474844db94
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-29 14:43:31 +0400

    start-project

---
+++ src/pages/MonchStatistics/MonchStatistics.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './MonchStatistics.module.scss'
+
+type Props = {}
+
+const MonchStatistics = (props: Props) => {
+  return (
+    <div className={s.home}>Monch Statistics</div>
+  )
+}
