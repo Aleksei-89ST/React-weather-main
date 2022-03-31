8b4f2aaf5e3fab9cc2709925001889987c8b1f79
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-31 12:47:51 +0400

    finished the layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { Route, Switch } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { MonchStatistics } from "./pages/MonchStatistics/MonchStatistics";
 import { Header } from "./shared/Header/Header";
+import { Popup } from "./shared/Popup/Popup";
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/Monch-Statistics" exact component={MonchStatistics} />
-      </Switch>
+    <div className="clobal-container">
+      <Popup />
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Monch-Statistics" exact component={MonchStatistics} />
+        </Switch>
+      </div>
     </div>
   );
 }
